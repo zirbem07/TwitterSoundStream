75caf7bc8f94c8e2635f16060d69142572fc1e66
zirbem07 <maxwell.zirbel@gmail.com>
2014-09-02 16:22:19 -0700

    changed path to app.log

--- lib/configure_logger.rb
+++ lib/configure_logger.rb
@@ -1,6 +1,6 @@
 require 'logger'
 
-log_file = File.open("./log/app.log", 'w')
+log_file = File.open("./lib/log/app.log", 'w')
 log_file.sync = true
 $logger = Logger.new log_file
 $logger.level = Logger::INFO
